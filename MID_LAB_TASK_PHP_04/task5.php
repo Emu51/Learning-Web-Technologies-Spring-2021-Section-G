fb968b9ee145e8ef542a59dab0594a68248ce652
Emu51 <emu.emu750@gmail.com>
2021-02-16 08:16:28 +0600

    added task 5

---
+++ MID_LAB_TASK_PHP_04/task5.php
@@ -0,0 +1,19 @@
+<?php
+
+    
+
+    for($num = 10; $num <=100; $num++)
+    {
+        if($num % 2 == 0)
+        {
+            continue;
+        }
+    
+        else
+        {
+            echo $num."<br>";
+        }
+    }
+
+
+?>
