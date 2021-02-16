81143dcde4a0a1d254a7090b96ef1ad31cda0216
Emu51 <emu.emu750@gmail.com>
2021-02-16 11:01:36 +0600

    Task 3-a

---
+++ MID_LAB_TASK_PHP_05/TASK_3/TASK_A/Submission Result.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+	if(isset($_POST['submit'])){
+
+		$Date=  $_POST['Date'];
+		
+
+		if(isset($_POST['submit'])){
+			echo "Sucess";
+		}else{
+			echo "Null submission...";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?> 
