664c81d938d986f4b731214b7cf94687792796c7
Emu51 <emu.emu750@gmail.com>
2021-02-16 08:13:54 +0600

    added task 3

---
+++ MID_LAB_TASK_PHP_04/task3.php
@@ -0,0 +1,14 @@
+<?php
+
+    $number = 22;
+    if($number % 2 == 0)
+    {
+        echo $number." is an even number";
+    }
+
+    else
+    {
+        echo $number." is an odd number";
+    }
+
+?>
