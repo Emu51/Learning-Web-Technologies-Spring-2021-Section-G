13587eca1e8ea443e27cd6231215bbca5a525392
Emu51 <emu.emu750@gmail.com>
2021-02-24 13:44:52 +0600

    finally fixed that task3 and its checking system

--- MID_LAB_TASK__PHP_06/profile_picture_check.php
+++ MID_LAB_TASK__PHP_06/profile_picture_check.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
     
-    $fileinfo = $_FILES["Image"]["name"];
+   
 
     
     $allowed_image_extension = array(
@@ -14,20 +14,25 @@ if (isset($_POST["submit"])) {
     $file_extension =$_FILES["Image"]["type"];
     
     
-    if (! file_exists($_FILES["Image"]["name"])) {
-        echo "Please upload a file";
-        ;
-    }    
-    else if (! in_array($file_extension, $allowed_image_extension)) {
-                echo "the file is in correct format";
+       
+     if (! in_array($file_extension, $allowed_image_extension) ) {
+                echo "the file is not in correct format";
         
     }    
     else if ($_FILES["Image"]["size"]/1000 > 4000) {
                 echo "upload a picture less than 4mb size";;
     }
-    else {
+    else if ((in_array($file_extension, $allowed_image_extension)) && ($_FILES["Image"]["size"]/1000 < 4000))
+        {
+        echo "The image is in correct format and size ";
+        echo "Profile Picture Uploaded Successfully"; 
+        }   
+
+    }
+    else    
+    {
         echo "picture upload successfull";
     }    
 
-}
+
 ?>
