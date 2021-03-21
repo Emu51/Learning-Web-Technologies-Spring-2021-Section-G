c86d2e198ba247800739b546d28480740b80ca33
Emu51 <emu.emu750@gmail.com>
2021-03-21 11:59:09 +0600

    user list updated

---
+++ Final_Lab_Task_01_DB/view/user_list.php
@@ -0,0 +1,36 @@
+<?php
+	$title = "User List Page";
+	include('header.php');
+	require_once('../model/db.php');
+
+
+	<a href="home.php">Back</a> |
+	<a href="../controller/logout.php">logout</a>	
+	<br>
+	
+	<h1>User list</h1>
+	$user = getAllUser();
+
+	echo "<table border=1>
+			<tr>
+				<td>ID</td>
+				<td>Username</td>
+				<td>Email</td>
+				<td>Type</td>
+			</tr>";
+
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		echo 	"<tr>
+					<td>{$row['id']}</td>
+					<td>{$row['username']}</td>
+					<td>{$row['email']}</td>
+					<td>{$row['type']}</td>
+				</tr>";
+	}
+
+	echo "</table>";
+
+
+	include('footer.php');
+?>
