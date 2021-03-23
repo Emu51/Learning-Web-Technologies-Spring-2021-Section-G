6a174faebe4fbb35c7195b63d1c96a4d870c48a2
Emu51 <emu.emu750@gmail.com>
2021-03-23 22:16:31 +0600

    added checking product back end

---
+++ FINAL_LAB_TASK_02_PHP/controller/search_product_check.php
@@ -0,0 +1,31 @@
+<?php
+        require_once('../Model/db.php');
+        require_once('../Model/product.php');
+        if(isset($_POST["submit"]))
+        {
+            $name=$_POST["name"];
+            $conn = getConnection();
+            $query="select * from products where name='{$name}' ";
+            $result= mysqli_query($conn,$query);
+            echo "<h1> Product List </h1>";
+            echo "<hr>";
+    
+            echo "<table border=2 width=700> <tr> <td> ID</td> <td> name </td> <td> buying price </td> <td> selling price </td>  <td> profit  </td> </tr> ";
+            while ($data = mysqli_fetch_assoc($result))
+            {
+               echo "<tr>
+                        <td>{$data['id']} </td>
+                        <td> {$data['name']} </td>
+                        <td> {$data['buying price']} </td>
+                        <td> {$data['selling price']} </td>
+                        <td> {$data['profit']} </td>
+                    </tr>";
+                    
+               echo "<br>";
+            }
+            echo "</table>";
+           
+           
+        }
+
+?>
